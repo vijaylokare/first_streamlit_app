c21dc6de9e32fab6c15e3e60a6c18167104df63b
vijaylokare <104962083+vijaylokare@users.noreply.github.com>
2022-05-05 10:27:13 +0530

    Update streamlit_app.py
    Added new changes for text and title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.title('My Mom\'s new healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
