0fc8eab4dd4781f2073582677229d9dc2be31248
vijaylokare <104962083+vijaylokare@users.noreply.github.com>
2022-05-09 08:35:48 +0530

    Added new import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My Mom\'s new healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
